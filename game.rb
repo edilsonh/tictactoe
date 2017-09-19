e167f3b79f1915dd47d6f85f79110916dcd81baa
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-09-19 11:30:51 -0400

    created an array for each corresponding position on the table, created user input to put an x in two positions, put a safeguard against invalid input

--- game.rb
+++ game.rb
@@ -1,10 +1,45 @@
 require 'terminal-table'
+a = []
+b = []
+c = []
 
-table = Terminal::Table.new do |t|
-  t << ['o','x','o']
-  t << :separator
-  t.add_row ['x','o','x']
-  t.add_separator
-  t.add_row ['o','x','o']
+for i in 1..3
+  a[i] = ' '
+  b[i] = ' '
+  c[i] = ' '
 end
-puts table
+
+while true
+  table = Terminal::Table.new do |t|
+    t << [a[1], a[2] ,a[3]]
+    t << :separator
+    t.add_row [b[1], b[2], b[3]]
+    t.add_separator
+    t.add_row [c[1], c[2], c[3]]
+  end
+  puts table
+
+  puts 'where you want the X at'
+
+  while true
+    answer = gets
+
+    if answer.chomp == 'a1'
+      break
+    elsif answer.chomp == 'b1'
+      break
+    elsif
+      puts "invalid response, try again"
+    end
+  end
+
+  for i in 1..3
+    if answer.chomp == "a#{i}"
+      a[i] = 'x'
+    elsif answer.chomp == "b#{i}"
+      b[i] = 'x'
+    elsif answer.chomp == "c#{i}"
+      c[i] = 'x'
+    end
+  end
+end
